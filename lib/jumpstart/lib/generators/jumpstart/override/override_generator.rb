46a81c01a68a688109619ee8b9f60055162d8f36
Chris Oliver <excid3@gmail.com>
2025-12-12 14:19:15 -0600

    Add jumpstart:override generator and move others to the same folder

---
+++ lib/jumpstart/lib/generators/jumpstart/override/override_generator.rb
@@ -0,0 +1,20 @@
+class Jumpstart::OverrideGenerator < Rails::Generators::NamedBase
+  source_root Jumpstart::Engine.root
+
+  argument :paths, type: :array, banner: "path path"
+
+  def self.usage_path = File.expand_path("USAGE", __dir__)
+
+  def copy_paths
+    paths.each do |path|
+      directory?(path) ? directory(path) : copy_file(path)
+    end
+  end
+
+  private
+
+  def directory?(path)
+    Dir.exist? find_in_source_paths(path)
+  end
+
+end
