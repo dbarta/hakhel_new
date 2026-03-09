114a585029df2ffad5af9b8e08c3c2987a1e904f
David Barta <david@odeca.net>
2026-03-09 19:52:34 +0200

    remove quotes for compatibility with heroku

--- script/07_initialize_db_with_admin.sh
+++ script/07_initialize_db_with_admin.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$ROOT"
 
-bin/rails runner 'load Rails.root.join("script","hke","init_db_with_admin.rb")'
+echo "@@@ Running script/hke/init_db_with_admin.rb"
+
+bin/rails runner script/hke/init_db_with_admin.rb
